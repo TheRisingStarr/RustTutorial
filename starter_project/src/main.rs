754c406a91e5ffb1e71190ee0e01136a6ca4d8be
Vedant Singh <ani.vedant@outlook.com>
2026-09-21 12:38:27 +0530

    Add Data Types

--- starter_project/src/main.rs
+++ starter_project/src/main.rs
@@ -4,13 +4,33 @@ fn main(){
     println!("Hello Rust"); 
     print!("Hello Rust1");
 
-    //Mutable variables
+//Mutable variables
     let mut x=23;
     println!("Before: {}",x);
     x=90;
     print!("After: {}",x);
 
-    //Data Types
+//Data Types
+    let num = 90;         // integer
+    let double = 0.9;     // float
+    let letter = 'P';     // character
+    let boole = false;    // boolean
+    let text = "Rust";    // string
+
+// Define Explicitly
+    let no: i32= 90;
+    let f: f64= 2.3;
+    let l: char= 'IO';
+    let b: bool= true;
+    let s: &str = "Hi";
+
+/*
+Data Types Groups:
+    Numbers- whole no and decimals (i32, f64)
+    Strings- Sequence of characters (&str)
+    Character- Single symbols or letters (char)
+    Boolean- True or False (bool)
+*/
 }
 
 
