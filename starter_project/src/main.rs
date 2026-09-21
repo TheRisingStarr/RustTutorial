aa5a0b716bd16da9c77bac818fc95cb798749b98
Vedant Singh <ani.vedant@outlook.com>
2026-09-21 12:21:08 +0530

    Mutable Variables

--- starter_project/src/main.rs
+++ starter_project/src/main.rs
@@ -3,4 +3,14 @@ fn main(){
     //println! and print! are macros
     println!("Hello Rust"); 
     print!("Hello Rust1");
+
+    //Mutable variables
+    let mut x=23;
+    println!("Before: {}",x);
+    x=90;
+    print!("After: {}",x);
+
+    //Data Types
 }
+
+
